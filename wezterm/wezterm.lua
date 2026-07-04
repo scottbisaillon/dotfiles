0a780ec671b76ab836ecd52ee525a3160ee79e24
Scott Bisaillon <scott.bisaillon.dev@gmail.com>
2026-07-04 13:43:14 -0400

    wezterm: enable kitty keyboard protocol for Ctrl chords

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,6 +4,11 @@ local config = {}
 config.default_prog = { 'pwsh.exe', '-NoLogo' }
 
 config.enable_tab_bar = false
+
+-- Honor the Kitty Keyboard Protocol so apps can disambiguate keychords like
+-- <C-Space>, <C-i>/<Tab>, <C-/> etc. Off by default in WezTerm; without it
+-- Ctrl+Space is sent as a NUL byte and never reaches Neovim as <C-Space>.
+config.enable_kitty_keyboard = true
 config.font = wezterm.font('JetBrainsMono Nerd Font')
 config.font_size = 14.0
 config.color_scheme = 'GruvboxDark'
