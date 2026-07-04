c173ac2b5d2cc8381011420882ff56e359cbe75c
Scott Bisaillon <scott.bisaillon.dev@gmail.com>
2026-07-04 12:47:58 -0400

    nvim: disable go.nvim default LSP keymaps to stop clobbering <leader> maps

--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -4,4 +4,8 @@ vim.pack.add { gh 'ray-x/go.nvim' }
 
 require('go').setup {
   lsp_cfg = true,
+  -- Don't install go.nvim's default LSP keymaps; they're buffer-local and
+  -- shadow our own bindings in Go files (e.g. <Leader>ff, the <Leader>e group,
+  -- and <C-k>). LSP actions live under <Leader>l (see lua/keymaps.lua).
+  lsp_keymaps = false,
 }
